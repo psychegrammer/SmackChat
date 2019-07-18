3b27de4f260501e21550ccd24fc2a8c0369a930b
stafamucalju <ximuboo@gmail.com>
2019-07-18 23:02:02 +0200

    lesson 12

--- app/src/main/java/psychegrammer/example/smack/Utilities/Constants.kt
+++ app/src/main/java/psychegrammer/example/smack/Utilities/Constants.kt
@@ -3,4 +3,7 @@ package psychegrammer.example.smack.Utilities
 const val BASE_URL = "https://smack020.herokuapp.com/"
 const val URL_REGISTER = "${BASE_URL}v1/account/register"
 const val URL_LOGIN = "${BASE_URL}v1/account/login"
-const val URL_CREATE_USER = "${BASE_URL}v1/user/add"
+const val URL_CREATE_USER = "${BASE_URL}v1/user/"
+
+// Broadcast Constants
+const val BROADCAST_USER_DATA_CHANGE = "BROADCAST_USER_DATA_CHANGE"
